2d29b7c311e7133fe324fdfce1e003c8a41a879b
MariaManuela <patrumariamanuela@gmail.com>
2023-01-26 21:30:46 +0200

    modified import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import Navigation from "./components/Navigation.jsx";
-import GetMovies from "./components/Movies.jsx";
+import Movies from "./components/Movies.jsx";
 import "./App.scss";
-import MoviePicture from "./components/MoviePicture.jsx";
 
 function App() {
   return (
